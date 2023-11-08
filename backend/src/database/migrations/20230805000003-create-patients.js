dfc2ad4024d547734c295fd165d0a75a79870fcc
matheusbwv <matheus.wenes@gmail.com>
2023-11-08 19:34:56 -0300

    fix:backend-docker compose

--- backend/src/database/migrations/20230805000003-create-patients.js
+++ backend/src/database/migrations/20230805000003-create-patients.js
@@ -29,7 +29,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      convenio: {
+      contact: {
         type: Sequelize.STRING,
         allowNull: false,
       },
